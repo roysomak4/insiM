91dea9a8b62632e2979d7ed34fc0206cb8f57660
Somak Roy <roysomak4@gmail.com>
2022-09-21 08:44:11 -0400

    Updated code to address python3 features and handle float to int conversion issues

--- insiM_v1.0.py
+++ insiM_v1.0.py
@@ -2,16 +2,11 @@
 
 import argparse
 import os
-import re
 import sys
 import random
 import itertools
-from collections import defaultdict
-import operator
-from operator import itemgetter
 import pysam
 import datetime
-import time
 from time import gmtime, strftime
 from array import *
 
@@ -60,7 +55,7 @@ def main(assay, bamfile, targetbed, mutfraction, outputfastqname, indellength, i
 		sys.exit(1)
 	
 	if not ((mutationtype in list(caseIgnore('snv'))) or (mutationtype in list(caseIgnore('mix')))):
-		print("mutationtype is %s" %mutationtype)
+		print(f"mutationtype is {mutationtype}")
 		try:
 			indellen = int(indellength)
 		except:
@@ -110,7 +105,7 @@ def main(assay, bamfile, targetbed, mutfraction, outputfastqname, indellength, i
 	current_chrom = "null"
 	for line in genome_input:
 		if line.startswith(">"):
-			current_chrom = line.rstrip().split(">")[1]
+			current_chrom = line.strip().split(' ')[0].strip('>')
 			genome_lists[current_chrom] = []
 		else:
 			genome_lists[current_chrom].append(line.rstrip().upper())
@@ -176,7 +171,9 @@ def main(assay, bamfile, targetbed, mutfraction, outputfastqname, indellength, i
 		else: 
 			mutation_spots.append(result)
 
-	chrList = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY" ]
+	# chrList = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "chrY" ]
+	chrList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y" ]
+	
 	
 	for i in range(0, 24):
     	
@@ -204,7 +201,7 @@ def main(assay, bamfile, targetbed, mutfraction, outputfastqname, indellength, i
 		for mutation_loc in mutation_spots:
 			mutation_chrom = mutation_loc.split('\t')[0]
 			if mutation_chrom == chrList[i]:
-				mutation_start = int(mutation_loc.split('\t')[1])
+				mutation_start = int(float(mutation_loc.split('\t')[1]))
 				mutation_end = mutation_start + 1
 
 				if mutationtype in list(caseIgnore("del")) and amplicon_flag==True and ((mutation_start + indellen) >= checkPosBed(mutation_chrom,mutation_start,amplicon)[2]):
@@ -412,7 +409,7 @@ def configParser(file):
 			params["seq"]	= str(params["seq"])
 		if params["out"]:
 			params["out"]	= str(params["out"])
-
+		print(params)
 		return params
 
 	except:
@@ -527,7 +524,7 @@ def validSequence(s):
 def get_mut_location(bedentry):
 	entryitems = bedentry.split('\t')
 	entrychrom = entryitems[0]
-	mutposition = int(entryitems[1]) + (int(entryitems[2]) - int(entryitems[1]))/2
+	mutposition = int(entryitems[1]) + int((int(entryitems[2]) - int(entryitems[1]))/2)
 	return entrychrom + '\t' + str(mutposition)
 
 def insert(sequence, position, insertseq):
@@ -743,11 +740,11 @@ def snv(sequence, readposition, genome, chrom, chromposition, alt):
 		return sequence
 
 
-def get_mut_location(bedentry):
-	entryitems = bedentry.split('\t')
-	entrychrom = entryitems[0]
-	mutposition = int(entryitems[1]) + (int(entryitems[2]) - int(entryitems[1]))/2
-	return entrychrom + '\t' + str(mutposition)
+# def get_mut_location(bedentry):
+# 	entryitems = bedentry.split('\t')
+# 	entrychrom = entryitems[0]
+# 	mutposition = int(entryitems[1]) + (int(entryitems[2]) - int(entryitems[1]))/2
+# 	return entrychrom + '\t' + str(mutposition)
 
 
 def readAmpBed(file):
